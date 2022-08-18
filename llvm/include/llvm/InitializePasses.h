4b37f17cea5c6b7dda882bb50f5a26966f70a988
Sami Tolvanen <samitolvanen@google.com>
2022-08-18 23:53:09 +0000

    Add generic KCFI operand bundle lowering
    The KCFI sanitizer emits "kcfi" operand bundles to indirect
    call instructions, which the LLVM back-end lowers into an
    architecture-specific type check with a known machine instruction
    sequence. Currently, KCFI operand bundle lowering is supported only
    on 64-bit X86 and AArch64 architectures.
    
    As a lightweight forward-edge CFI implementation that doesn't
    require LTO is also useful for non-Linux low-level targets on
    other machine architectures, add a generic KCFI operand bundle
    lowering pass that's only used when back-end lowering support is not
    available and allows -fsanitize=kcfi to be enabled in Clang on all
    architectures.
    
    This relands commit eb2a57ebc7aaad551af30462097a9e06c96db925 with
    fixes.
    
    Reviewed By: nickdesaulniers, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D135411

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -268,6 +268,7 @@ void initializeLowerInvokeLegacyPassPass(PassRegistry&);
 void initializeLowerSwitchLegacyPassPass(PassRegistry &);
 void initializeLowerMatrixIntrinsicsLegacyPassPass(PassRegistry &);
 void initializeLowerMatrixIntrinsicsMinimalLegacyPassPass(PassRegistry &);
+void initializeKCFIPass(PassRegistry &);
 void initializeMIRAddFSDiscriminatorsPass(PassRegistry &);
 void initializeMIRCanonicalizerPass(PassRegistry &);
 void initializeMIRNamerPass(PassRegistry &);
